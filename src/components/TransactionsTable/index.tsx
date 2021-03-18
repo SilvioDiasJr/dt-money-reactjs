ad91df1ada771138afe39a59b07aa070e9801795
Silvio <silvio.dias@etec.sp.gov.br>
2021-03-18 00:56:30 -0300

    adicionar funcao e interface

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction {
+  id: number
+  title: string
+  amount: number
+  type: string
+  category: string
+  createdAt: string
+}
+
 export function TransactionsTable(){
+  const [trasactions, setTransactions] = useState<Transaction[]>([])
+
+  useEffect(() => {
+    api.get('/transactions')
+      .then(response => setTransactions(response.data.transactions))
+  }, [])
+  
   return(
     <Container>
       <table>
@@ -14,19 +32,24 @@ export function TransactionsTable(){
         </thead>
 
         <tbody>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="deposit">R$ 12.000</td>
-            <td>Desenvolvimento</td>
-            <td>17/03/2021</td>
-          </tr>
-
-          <tr>
-            <td>Parcela</td>
-            <td className="withdraw">- R$ 900</td>
-            <td>Apartamento</td>
-            <td>10/03/2021</td>
-          </tr>
+          {trasactions.map(transaction => {
+            return(
+              <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('pt-BR',{
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(transaction.amount)}
+                </td>
+              <td>{transaction.category}</td>
+              <td>
+                {new Intl.DateTimeFormat('pt-BR')
+                .format(new Date(transaction.createdAt))}
+              </td>
+            </tr>
+            )})
+          }
         </tbody>
       </table>
     </Container>
